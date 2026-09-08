5d13517be9676e99298c26ceb0a9486e293e1691
Omar.Albaz <mralehsas@gmail.com>
2026-09-08 08:47:31 +0300

    Refresh cache for ALBAZ Smart Atlas mobile deployment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'albaz-ne-v2-2-v3-index-hardfix';
+const CACHE_NAME = 'albaz-ne-v2-2-v4-smart-atlas-android-ui';
 const STATIC_ASSETS = [
   './site.webmanifest',
   './ALBAZ_NE_V2_2_FULL_CRITERION_AR.pdf'
