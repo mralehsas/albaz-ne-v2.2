3e58520592f262bdb851c0fc913b62d0260dd303
Omar.Albaz <mralehsas@gmail.com>
2026-09-08 10:46:07 +0300

    Refresh PWA cache after startup and export cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'albaz-official-flagship-v9-2026-09-08-resolution-lock';
+const CACHE_NAME = 'albaz-official-flagship-v10-2026-09-08-clean-startup';
 const APP_SHELL = [
   './',
   './index.html',
